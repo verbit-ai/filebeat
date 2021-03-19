429c48ea6ef62a972ca269e9338109c27a4cb113
Mikhail Vasilenko <mikhailv@verbit.ai>
2021-03-19 13:12:56 +0200

    add field with instance type

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 # we need "external" private instance IP as elasticsearch field
 IP_ADDRESS=$(curl -qs http://169.254.169.254/latest/meta-data/local-ipv4)
 EC2_INSTANCE_ID=$(curl -qs http://169.254.169.254/latest/meta-data/instance-id)
+EC2_INSTANCE_TYPE=$(curl -qs http://169.254.169.254/latest/meta-data/instance-type)
 if  ! [[ ${IP_ADDRESS} =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
 then
   IP_ADDRESS="0.0.0.0"
@@ -19,9 +20,10 @@ fi
 
 export IP_ADDRESS=${IP_ADDRESS}
 export EC2_INSTANCE_ID=${EC2_INSTANCE_ID}
+export EC2_INSTANCE_TYPE=${EC2_INSTANCE_TYPE}
 export ES_INDEX_PREFIX=${ES_INDEX_PREFIX}
 export FILEBEAT_VERSION=${FILEBEAT_VERSION}
 
-echo EC2_INSTANCE_ID $EC2_INSTANCE_ID IP_ADDRESS ${IP_ADDRESS} ES_INDEX_PREFIX ${ES_INDEX_PREFIX} FILEBEAT_VERSION ${FILEBEAT_VERSION}
+echo EC2_INSTANCE_ID $EC2_INSTANCE_ID EC2_INSTANCE_TYPE $EC2_INSTANCE_TYPE IP_ADDRESS ${IP_ADDRESS} ES_INDEX_PREFIX ${ES_INDEX_PREFIX} FILEBEAT_VERSION ${FILEBEAT_VERSION}
 envsubst < /filebeat.yml > /usr/share/filebeat/filebeat.yml
 /usr/share/filebeat/filebeat -e -c /usr/share/filebeat/filebeat.yml
